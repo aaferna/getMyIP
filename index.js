c848a8a1524e72af1957698a70737ce8d7cac634
Agustin Fernandez <aaferna@outlook.com.ar>
2021-09-17 19:26:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const path = require('path');
 const deployPath = path.dirname(process.execPath);
 require('dotenv').config();
-
 path.join(deployPath, "/.env")
 let tetok = process.env.TELEGTOK
 const { Telegraf } = require('telegraf')
 bot = new Telegraf(process.env.TELEGTOK)
+
+
 bot.catch((err, ctx) => {
     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
 })
+
 bot.command('chatIDclog', (ctx) => {
     console.log(ctx.message.chat.id)
 })
+
 bot.command('buscarip', (ctx) => {
-    const axios = require('axios');
-    axios({
-        method: 'get',
-        url: 'http://ipinfo.io/ip'
-      })
-    .then((response) => { bot.telegram.sendMessage(process.env.TELEGchat, response.data)})
-    .catch((error) => { console.log(error); });
+    
+    try {
+        const axios = require('axios');
+        axios({
+            method: 'get',
+            url: 'http://ipinfo.io/ip'
+        })
+        .then((response) => { 
+                bot.telegram.sendMessage(process.env.TELEGchat, response.data)
+        })
+        .catch((error) => { console.log(error); });
+    } catch (error) {
+        console.log(error);
+    }
+
 })
+
+
+
 bot.launch()
